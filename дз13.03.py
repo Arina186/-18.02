1685ffa64fd756724f76123d9b3831b9c24d7638
Arina186 <ariadna.sem@mail.ru>
2026-03-16 15:21:20 +0300

    Task 3, дз13.03 повторяющиеся слова

--- дз13.03.py
+++ дз13.03.py
@@ -1,8 +1,8 @@
 # task 1
 import os, platform
 
-print(f"Operational system: {platform.system()} {os.name}") #имя операционной системы
-print(f"Current working directory: {os.getcwd()}") #путь до текущей папки
+print(f"Operational system: {platform.system()} {os.name}")  # имя операционной системы
+print(f"Current working directory: {os.getcwd()}")  # путь до текущей папки
 
 current_files = os.listdir()
 
@@ -11,7 +11,7 @@ for file in current_files:
     if file.count(".") == 1:
         file_parts = file.split(".")
         if file_parts[0] != "" and file_parts[1] != "":
-           extensions.add(file_parts[1].lower())
+            extensions.add(file_parts[1].lower())
 
 print(extensions)
 # сгруппировать файлы в папку
@@ -20,8 +20,8 @@ for ext in extensions:
         new_folder_name = f"{ext}s"
         if not os.path.exists(new_folder_name):
             os.mkdir(new_folder_name)
-        count = 0 # сколько файлов в папке
-        total_size = 0 # для размера в гб
+        count = 0  # сколько файлов в папке
+        total_size = 0  # для размера в гб
         for file in current_files:
             if file.count(".") == 1:
                 file_parts = file.split(".")
@@ -29,11 +29,11 @@ for ext in extensions:
                     file_path = os.path.join(os.getcwd(), file)
                     total_size += os.path.getsize(file_path)
                     count += 1
-                    replaced_file_path = os.path.join(os.getcwd(), new_folder_name, file) #file - новое имя файла
-                    os.replace(file,replaced_file_path)
+                    replaced_file_path = os.path.join(os.getcwd(), new_folder_name, file)  # file - новое имя файла
+                    os.replace(file, replaced_file_path)
         if count > 0:
-           size_file = total_size/1024**3
-           print(f"В папке с {ext} файлами перемещено {count} файлов, их суммарный размер - {size_file:.10f} ГБ")
+            size_file = total_size / 1024 ** 3
+            print(f"В папке с {ext} файлами перемещено {count} файлов, их суммарный размер - {size_file:.10f} ГБ")
         # .5f где 5-кол-во знаков после запятой f — тип данных fixed-point (число с фиксированной точкой)
         else:
             print(f"Для расширения {ext} файлов не найдено (возможно, уже перемещены)")
@@ -46,14 +46,41 @@ if "." in target_file:
     folder_of_file = f"{ext}s"
     old_path = os.path.join(os.getcwd(), folder_of_file, target_file)
     new_path = os.path.join(os.getcwd(), folder_of_file, new_file)
-    #Проверяем наличие и переименовываем
+    # Проверяем наличие и переименовываем
     if os.path.exists(old_path):
         os.rename(old_path, new_path)
         print(f"File {target_file} was renamed to {new_file}")
     else:
         print(f"File {target_file} was not found in {folder_of_file}")
 
-#task 2
+# task 3
 
+input_path = os.path.join("txts", "text.txt")
+output_path = os.path.join("txts", "output.txt")
+try:
+    with open(input_path, "r", encoding="utf-8") as file, open(output_path, "w", encoding="utf-8") as output:
+        for line in file:
+            word_count = line.lower().split()
+            if not word_count:  # Если строка пустая — пропускаем
+                continue
+            counts = {}
+            for word in word_count:
+                clean_word = word.strip(".,!?;:-")
+                if clean_word in counts:
+                    counts[clean_word] += 1  # Если слово уже записано, мы просто увеличиваем число рядом с ним на +1.
+                else:
+                    counts[
+                        clean_word] = 1  # Если слова еще нет, мы записываем его в книжку и ставим цифру 1, так как мы увидели его в первый раз.
 
+            max_word = ""
+            max_count = 0
+            for word in counts:
+                if counts[word] > max_count:
+                    max_count = counts[word]
+                    max_word = word
+            output.write(f"{max_word} {counts[max_word]}\n")
 
+except Exception as e:
+    print(f"Error: {e}")
+
+# task 4
